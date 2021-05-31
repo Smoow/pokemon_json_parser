da8b5c74dc1d2e36c12c72e0ff54416ea7375cf5
Pedro H. C <pedrohcm5@gmail.com>
2021-05-31 08:43:22 -0300

    Removing unused library and adding std namespace

--- poke-parser.cpp
+++ poke-parser.cpp
@@ -1,21 +1,21 @@
 #include <nlohmann/json.hpp>
-#include <vector>
 #include <fstream>
 #include <iostream>
 
+using namespace std;
 using nlohmann::json;
 
 int main()
 {
-    std::ifstream data("pokedex.json");
+    ifstream data("pokedex.json");
     json jsonData = json::parse(data);
     int it = 0;
 
     while(!jsonData[it]["id"].is_null()) {
-        std::cout << "id: " << jsonData[it]["id"] << std::endl;
-        std::cout << "Name: " << jsonData[it]["name"]["english"] << std::endl;
-        std::cout << "Type: " << jsonData[it]["type"] << std::endl;
-        std::cout << "Base: " << jsonData[it]["base"] << std::endl << std::endl;
+        cout << "id: " << jsonData[it]["id"] << endl;
+        cout << "Name: " << jsonData[it]["name"]["english"] << endl;
+        cout << "Type: " << jsonData[it]["type"] << endl;
+        cout << "Base: " << jsonData[it]["base"] << endl << endl;
         it++;
     }
 
